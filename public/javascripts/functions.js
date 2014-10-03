f3340c4f0dc9eabf19ae8f1d08477db949ec5b43
Rui Reis <dante2308@gmail.com>
2014-10-03 14:26:38 +0200

    sa march

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -3,6 +3,15 @@ console.log("bonjour");
 function githubSearch() {
     var textField = document.getElementById('search-field').value;
 
+    //https://api.github.com/search/repositories?q=textField
+
+    $.get("https://api.github.com/search/repositories?q=" + textField, function (data) {
+        $(".result").html(data);
+        console.log(data);
+        alert("Load was performed.");
+        alert(data);
+    });
+
     console.log(textField);
 }
 
